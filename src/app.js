8a2677b074420cc3eff73662fe308e3891a4234b
pimurphs <pi.rodrigues@live.com>
2022-03-07 15:39:48 +0000

    Added search button - API search

--- src/app.js
+++ src/app.js
@@ -46,9 +46,20 @@ function displayTemperature(response) {
 }
 
 //Axios API
+function search(city) {
+  let apiKey = "8cb4b09ce92dcd0f47ea25293231322e";
+  let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=metric`;
 
-let apiKey = "8cb4b09ce92dcd0f47ea25293231322e";
-let city = "London";
-let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=metric`;
+  axios.get(apiUrl).then(displayTemperature);
+}
 
-axios.get(apiUrl).then(displayTemperature);
+function handleSubmit(event) {
+  event.preventDefault();
+  let cityInputElement = document.querySelector("#city-input");
+  search(cityInputElement.value);
+}
+
+search("Lisbon");
+
+let form = document.querySelector("#search-form");
+form.addEventListener("submit", handleSubmit);
