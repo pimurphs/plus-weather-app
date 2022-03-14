9fca7e64a86654485dca3aad1577f8c7bc6a6ef7
pimurphs <pi.rodrigues@live.com>
2022-03-14 16:33:13 +0000

    Added JS to change background (needs fixing)

--- src/app.js
+++ src/app.js
@@ -41,11 +41,11 @@ function displayForecast(response) {
   let forecastHTML = `<div class ="row">`;
 
   forecast.forEach(function (forecastDay, index) {
-    if (index < 5) {
+    if (index > 0 && index < 6) {
       forecastHTML =
         forecastHTML +
         `
-        <div class="col-2">
+        <div class="col">
           <div class="weather-forecast-date">
           ${formatDay(forecastDay.dt)}
           </div>
@@ -73,7 +73,7 @@ function getForecast(coordinates) {
   console.log(coordinates);
   let apiKey = "8cb4b09ce92dcd0f47ea25293231322e";
   let apiUrl = `https://api.openweathermap.org/data/2.5/onecall?lat=${coordinates.lat}&lon=${coordinates.lon}&appid=${apiKey}&units=metric`;
-  console.log(apiUrl);
+
   axios.get(apiUrl).then(displayForecast);
 }
 
@@ -163,6 +163,37 @@ function showPosition(position) {
 function getCurrentLocation(event) {
   event.preventDefault();
   navigator.geolocation.getCurrentPosition(showPosition);
+}
+
+function changingBackground(response) {
+  let descriptionElement = response.data.weather[0].description;
+
+  if (descriptionElement === "Clear Sky") {
+    document.body.style.background = `linear-gradient: (26deg, rgb(243, 248, 179) 15%, rgb(115, 235, 252) 97%) `;
+  } else if (descriptionElement === "Scattered Clouds") {
+    document.body.style.background = `linear-gradient: (26deg, rgb(166, 167, 155) 15%, rgb(115, 235, 252) 97%) `;
+  } else if (descriptionElement === "Few Clouds") {
+    document.body.style.background = `linear-gradient: (26deg, rgb(221, 221, 218) 15%, rgb(101, 180, 252) 97%) `;
+  } else if (descriptionElement === "Broken Clouds") {
+    document.body.style.background = `linear-gradient: (26deg, rgb(231, 232, 238) 46%, rgb(45, 128, 211) 100% `;
+  } else if (descriptionElement === "Shower Rain") {
+    document.body.style.background = `linear-gradient: (26deg, rgb(248, 156, 202) 20%, rgb(112, 118, 248) 100%) `;
+  } else if (descriptionElement === "Rain") {
+    document.body.style.background = `linear-gradient: (26deg, rgb(255, 250, 252) 17%, rgb(170, 168, 168) 100%) `;
+  } else if (descriptionElement === "Thunderstorm") {
+    document.body.style.background = `linear-gradient: (26deg, rgb(248, 248, 151) 28%, rgb(238, 183, 15) 80%) `;
+  } else if (descriptionElement === "Snow") {
+    document.body.style.background = `linear-gradient: (26deg, rgb(248, 245, 246) 26%, rgb(35, 231, 250) 86%) `;
+  } else if (descriptionElement === "Mist") {
+    document.body.style.background = `linear-gradient: (26deg, rgb(91, 160, 114) 26%, rgb(242, 246, 246) 75%) `;
+  }
+}
+
+function getDescription(coordinates) {
+  let apiKey = "8cb4b09ce92dcd0f47ea25293231322e";
+  let apiUrl = `https://api.openweathermap.org/data/2.5/weather?lat=${coordinates.lat}&lon=${coordinates.long}&appid=${apiKey}&units=metric`;
+
+  axios.get(apiUrl).then(changingBackground);
 }
 
 //Button for current location
